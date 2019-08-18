ea40e21f26e7c589126b80d3393e4d8d0a4ec1f9
Gashenko Anna <janngash@gmail.com>
2019-08-18 16:28:42 +0300

    Add signup ana login

--- source/bus/__DOMAIN__/saga/workers/worker.js
+++ source/bus/__DOMAIN__/saga/workers/worker.js
@@ -3,23 +3,21 @@ import { put, apply } from 'redux-saga/effects'
 
 // Instruments
 import { api } from '../../../../REST';
-import { postActions } from '../../actions';
 import { uiActions } from '../../../ui/actions';
 
-export function* fetchPosts () {
+export function* worker () {
     try {
         yield put(uiActions.startFetching());
 
         const response = yield apply(api, api.posts.fetch);
-        const { data: posts, message } = yield apply(response, response.json);
+        const { data: post, message } = yield apply(response, response.json);
 
         if(response.status !== 200) {
             throw new Error(message);
         }
 
-        yield put(postActions.fillPosts(posts));
     } catch (error) {
-        yield put(uiActions.emitError(error, '-> fetch worker'));
+        yield put(uiActions.emitError(error, '-> worker'));
     } finally {
         yield put(uiActions.stopFetching());
     }
