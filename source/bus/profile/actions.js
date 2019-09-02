8d2e847ea43a8a8a39bbe097627716b639e79fc4
Gashenko Anna <janngash@gmail.com>
2019-09-02 23:31:08 +0300

    Add update Profile:
    1. Update avatar
    2. Update password
    3. Update Name

--- source/bus/profile/actions.js
+++ source/bus/profile/actions.js
@@ -2,10 +2,18 @@
 import { types } from './types';
 
 export const profileActions = {
+    // Sync
     fillProfile: (profile) => {
         return {
             type: types.FILL_PROFILE,
             payload: profile,
+        }
+
+    },
+    updateAvatar: (nameAvatarUrl) => {
+        return {
+            type: types.UPDATE_AVATAR,
+            payload: nameAvatarUrl,
         }
 
     },
@@ -14,6 +22,26 @@ export const profileActions = {
             type: types.CLEAR_PROFILE,
         }
 
+    },
+
+    // Async
+    updateNameAsync: (newName) => {
+        return {
+            type: types.UPDATE_NAME_ASYNC,
+            payload: newName
+        }
+    },
+    updateAvatarAsync: (nameAvatar) => {
+        return {
+            type: types.UPDATE_AVATAR_ASYNC,
+            payload: nameAvatar
+        }
+    },
+    updatePasswordAsync: (passwordData) => {
+        return {
+            type: types.UPDATE_PASSWORD_ASYNC,
+            payload: passwordData
+        }
     },
 
 };
--- source/init/rootReducer.js
+++ source/init/rootReducer.js
@@ -8,6 +8,7 @@ import { uiReducer as ui } from '../bus/ui/reducer';
 import { authReducer as auth } from '../bus/auth/reducer';
 import { profileReducer as profile } from '../bus/profile/reducer';
 import { usersReducer as users } from '../bus/users/reducer';
+import { formsReducer as forms } from '../bus/forms/reducer';
 
 export const rootReducer = combineReducers({
     auth,
@@ -15,5 +16,6 @@ export const rootReducer = combineReducers({
     posts,
     profile,
     users,
-    router
+    router,
+    forms,
 });
